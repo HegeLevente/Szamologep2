9dc9c46f5d46099d7bba1afb8107f56d1dbc4638
HegeLevente <115217211+HegeLevente@users.noreply.github.com>
2023-03-02 20:10:38 +0100

    Add files via upload

---
+++ Számológép2/Számológép2/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Számológép2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.Button();
+            this.num3 = new System.Windows.Forms.Button();
+            this.num4 = new System.Windows.Forms.Button();
+            this.num5 = new System.Windows.Forms.Button();
+            this.num6 = new System.Windows.Forms.Button();
+            this.num7 = new System.Windows.Forms.Button();
+            this.num8 = new System.Windows.Forms.Button();
+            this.num9 = new System.Windows.Forms.Button();
+            this.num0 = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.minusz = new System.Windows.Forms.Button();
+            this.szorzas = new System.Windows.Forms.Button();
+            this.osztas = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.eredmeny = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(108, 58);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(39, 38);
+            this.num1.TabIndex = 0;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            this.num1.Click += new System.EventHandler(this.num1_Click);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(187, 58);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(39, 38);
+            this.num2.TabIndex = 1;
+            this.num2.Text = "2";
+            this.num2.UseVisualStyleBackColor = true;
+            this.num2.Click += new System.EventHandler(this.num2_Click);
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(263, 58);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(39, 38);
+            this.num3.TabIndex = 2;
+            this.num3.Text = "3";
+            this.num3.UseVisualStyleBackColor = true;
+            this.num3.Click += new System.EventHandler(this.num3_Click);
+            // 
+            // num4
+            // 
+            this.num4.Location = new System.Drawing.Point(108, 132);
+            this.num4.Name = "num4";
+            this.num4.Size = new System.Drawing.Size(39, 38);
+            this.num4.TabIndex = 3;
+            this.num4.Text = "4";
+            this.num4.UseVisualStyleBackColor = true;
+            this.num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num5
+            // 
+            this.num5.Location = new System.Drawing.Point(187, 132);
+            this.num5.Name = "num5";
+            this.num5.Size = new System.Drawing.Size(39, 38);
+            this.num5.TabIndex = 4;
+            this.num5.Text = "5";
+            this.num5.UseVisualStyleBackColor = true;
+            this.num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num6
+            // 
+            this.num6.Location = new System.Drawing.Point(263, 132);
+            this.num6.Name = "num6";
+            this.num6.Size = new System.Drawing.Size(39, 38);
+            this.num6.TabIndex = 5;
+            this.num6.Text = "6";
+            this.num6.UseVisualStyleBackColor = true;
+            this.num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // num7
+            // 
+            this.num7.Location = new System.Drawing.Point(108, 203);
+            this.num7.Name = "num7";
+            this.num7.Size = new System.Drawing.Size(39, 38);
+            this.num7.TabIndex = 6;
+            this.num7.Text = "7";
+            this.num7.UseVisualStyleBackColor = true;
+            this.num7.Click += new System.EventHandler(this.num7_Click);
+            // 
+            // num8
+            // 
+            this.num8.Location = new System.Drawing.Point(187, 203);
+            this.num8.Name = "num8";
+            this.num8.Size = new System.Drawing.Size(39, 38);
+            this.num8.TabIndex = 7;
+            this.num8.Text = "8";
+            this.num8.UseVisualStyleBackColor = true;
+            this.num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num9
+            // 
+            this.num9.Location = new System.Drawing.Point(263, 203);
+            this.num9.Name = "num9";
+            this.num9.Size = new System.Drawing.Size(39, 38);
+            this.num9.TabIndex = 8;
+            this.num9.Text = "9";
+            this.num9.UseVisualStyleBackColor = true;
+            this.num9.Click += new System.EventHandler(this.num9_Click);
+            // 
+            // num0
+            // 
+            this.num0.Location = new System.Drawing.Point(187, 280);
+            this.num0.Name = "num0";
+            this.num0.Size = new System.Drawing.Size(39, 38);
+            this.num0.TabIndex = 9;
+            this.num0.Text = "0";
+            this.num0.UseVisualStyleBackColor = true;
+            this.num0.Click += new System.EventHandler(this.num0_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(387, 58);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(39, 38);
+            this.plus.TabIndex = 10;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minusz
+            // 
+            this.minusz.Location = new System.Drawing.Point(387, 132);
+            this.minusz.Name = "minusz";
+            this.minusz.Size = new System.Drawing.Size(39, 38);
+            this.minusz.TabIndex = 11;
+            this.minusz.Text = "-";
+            this.minusz.UseVisualStyleBackColor = true;
+            this.minusz.Click += new System.EventHandler(this.minusz_Click);
+            // 
+            // szorzas
+            // 
+            this.szorzas.Location = new System.Drawing.Point(387, 203);
+            this.szorzas.Name = "szorzas";
+            this.szorzas.Size = new System.Drawing.Size(39, 38);
+            this.szorzas.TabIndex = 12;
+            this.szorzas.Text = "*";
+            this.szorzas.UseVisualStyleBackColor = true;
+            this.szorzas.Click += new System.EventHandler(this.szorzas_Click);
+            // 
+            // osztas
+            // 
+            this.osztas.Location = new System.Drawing.Point(387, 280);
+            this.osztas.Name = "osztas";
+            this.osztas.Size = new System.Drawing.Size(39, 38);
+            this.osztas.TabIndex = 13;
+            this.osztas.Text = "/";
+            this.osztas.UseVisualStyleBackColor = true;
+            this.osztas.Click += new System.EventHandler(this.osztas_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(547, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 145);
+            this.textBox1.TabIndex = 14;
+            // 
+            // eredmeny
+            // 
+            this.eredmeny.Location = new System.Drawing.Point(469, 58);
+            this.eredmeny.Name = "eredmeny";
+            this.eredmeny.Size = new System.Drawing.Size(39, 38);
+            this.eredmeny.TabIndex = 15;
+            this.eredmeny.Text = "=";
+            this.eredmeny.UseVisualStyleBackColor = true;
+            this.eredmeny.Click += new System.EventHandler(this.eredmeny_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(469, 132);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(47, 38);
+            this.clear.TabIndex = 16;
+            this.clear.Text = "Nulláz";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 501);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.eredmeny);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.osztas);
+            this.Controls.Add(this.szorzas);
+            this.Controls.Add(this.minusz);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.num0);
+            this.Controls.Add(this.num9);
+            this.Controls.Add(this.num8);
+            this.Controls.Add(this.num7);
+            this.Controls.Add(this.num6);
+            this.Controls.Add(this.num5);
+            this.Controls.Add(this.num4);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.Button num2;
+        private System.Windows.Forms.Button num3;
+        private System.Windows.Forms.Button num4;
+        private System.Windows.Forms.Button num5;
+        private System.Windows.Forms.Button num6;
+        private System.Windows.Forms.Button num7;
+        private System.Windows.Forms.Button num8;
+        private System.Windows.Forms.Button num9;
+        private System.Windows.Forms.Button num0;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minusz;
+        private System.Windows.Forms.Button szorzas;
+        private System.Windows.Forms.Button osztas;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button eredmeny;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ Számológép2/Számológép2/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Számológép2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int szam1;
+        int szam2;
+        string behoz;
+        char muvelet;
+        int vegeredmeny;
+        private void num1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "1";
+            behoz += "1";
+            
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2"; 
+            behoz += "2";
+           
+        }
+
+        private void num3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+            behoz+= "3";
+            
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+            behoz += "4";
+            
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+            behoz += "5";
+            
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+            behoz += "6";
+            
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+            behoz += "7";
+            
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+            behoz += "8";
+            
+        }
+
+        private void num9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+            behoz += "9";
+            
+        }
+
+        private void num0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+            behoz += "0";
+            
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "+";
+            szam1 = Convert.ToInt32(behoz);
+            muvelet = '+';
+            behoz = "";
+        }
+
+        private void minusz_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "-";
+            szam1 = Convert.ToInt32(behoz);
+            muvelet = '-';
+            behoz = "";
+        }
+
+        private void szorzas_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "*";
+            szam1 = Convert.ToInt32(behoz);
+            muvelet = '*';
+            behoz = "";
+        }
+
+        private void osztas_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "/";
+            szam1 = Convert.ToInt32(behoz);
+            muvelet = '/';
+            behoz = "";
+        }
+
+        private void eredmeny_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "=";
+            szam2 = Convert.ToInt32(behoz);
+            if (muvelet=='+')
+            {
+                vegeredmeny = szam1 + szam2;
+            }
+            if (muvelet == '-')
+            {
+                vegeredmeny = szam1 - szam2;
+            }
+            if (muvelet == '*')
+            {
+                vegeredmeny = szam1 * szam2;
+            }
+            if (muvelet == '/')
+            {
+                if (szam2 == 0)
+                {
+                    MessageBox.Show("0-val nem lehet osztani");
+                }
+                else
+                {
+                    vegeredmeny = szam1 / szam2;
+                }
+                
+            }
+            textBox1.Text = textBox1.Text + vegeredmeny;
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            szam1 = 0;
+            szam2 = 0;
+            behoz = "";
+        }
+    }
+}
